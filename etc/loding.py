9b5e94a0209546f6a083436985399bb6eba5586a
Avindeso <74377866+Fuziro@users.noreply.github.com>
2020-12-27 10:14:21 +0700

    Delete loding.py

--- etc/loding.py
+++
@@ -1,8 +0,0 @@
-import os, sys, threading
-
-from warn.warn import *
-
-def loding():
-
-    print (f"{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}•{p}•{b}")
-
